9d97f2c2dcec3e3e7c8efbe1b2952fcebdb7d330
Alex Jack <alex@Alexs-MacBook-Air.local>
2023-05-19 13:00:37 +0100

    6 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,15 +1,26 @@
 # WRITE YOUR FUNCTIONS HERE
 
+import pdb
+
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] += cash
+
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] += cash
+
+def get_pets_sold (sold):
+    return sold["admin"]["pets_sold"]
+
+
+
+
 def find_pet_by_name(pet_shop, pet_name):
     for pet in pet_shop["pets"]:
         if pet["name"] == pet_name:
             return pet
-
-
-    pass
