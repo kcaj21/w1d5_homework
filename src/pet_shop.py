803f9619e88d5fcbfcf1a27e0eb4b0680b6289b1
Alex Jack <alex@Alexs-MacBook-Air.local>
2023-05-19 15:05:34 +0100

    16 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -51,3 +51,6 @@ def get_customer_cash(customer):
 
 def remove_customer_cash(customer, amount):
     customer["cash"] -= amount
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
